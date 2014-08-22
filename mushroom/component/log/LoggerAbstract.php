d2411b469e0c6ed9f117231ea8b701ae0441cc35
kaleesi <85316319@qq.com>
2014-08-22 14:21:10 +0800

    增加 logger

---
+++ mushroom/component/log/LoggerAbstract.php
@@ -0,0 +1,50 @@
+<?php
+namespace mushroom\component\log;
+
+abstract LoggerAbstract implements LoggerInterface
+{
+
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const NOTICE    = 'notice';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const INFO      = 'info';
+
+    public function emergency($message, array $context = array()) 
+    {
+        $this->log(self::EMERGENCY, $message, $context);
+    
+    }
+
+    public function alert($message, array $context = array())
+    {
+        $this->log(self::ALERT, $message, $context);
+    
+    }
+
+    public function notice($message, array $context = array())
+    {
+        $this->log(self::NOTICE, $message, $context);
+        
+    }
+
+    public function error($message, array ,$context = array())
+    {
+        $this->log(self::ERROR, $message, $context);
+    
+    }
+
+    public function warning($message, array $context = array())
+    {
+        $this->log(self::WARNING, $message, $context);
+    
+    }
+
+    public function info($message, array $context = array())
+    {
+        $this->log(self::INFO, $message, $context);
+    
+    }
+}
+
