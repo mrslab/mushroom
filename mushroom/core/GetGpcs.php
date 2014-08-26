c89cdcbb6aee34b9e6bd29303d56f16f8e8d9e7b
Mengfk <dreamans@163.com>
2014-08-26 14:34:02 +0800

    统一文件注释信息

--- mushroom/core/GetGpcs.php
+++ mushroom/core/GetGpcs.php
@@ -1,14 +1,13 @@
 <?php
-
 /**
  * Mushroom
  * 
  * An open source php application framework for PHP 5.3.0 or newer
  *
  * @author    mengfk <Mushroom Dev Team>
- * @copyright Copyright (C) 2014 http://mushroom.dreamans.com All rights reserved.
+ * @copyright Copyright (C) 2014 <MrsLab Team> All rights reserved.
  * @license   http://www.apache.org/licenses/LICENSE-2.0.txt
- * @link      http://mushroom.dreamans.com
+ * @link      https://github.com/mrslab/mushroom
  */
 
 namespace mushroom\core;
